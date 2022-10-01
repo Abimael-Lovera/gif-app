9d6192e2dc34939a21b1528447b56b9178e93e1b
Abimael Lovera <abimaellovera@gmail.com>
2022-10-01 11:23:03 -0300

    Usando useState em formulario

--- src/GifApp.jsx
+++ src/GifApp.jsx
@@ -1,20 +1,22 @@
 import { useState } from 'react';
+import { AddCategory } from './components/AddCategory';
 
 export const GifApp = () => {
 	const [categories, setCategories] = useState(['One Punch', 'Dragon Ball']);
-	console.log(categories);
+
+	
 	return (
 		<>
 			<h1>GifApp</h1>
+
+				<AddCategory setCategories={ setCategories }/>
+
 			<ol>
 				{
 					categories.map((category) => {
 						return <li key={category}>{category}</li>;
 					})
 				}
-				<li>1</li>
-				<li>2</li>
-				<li>3</li>
 			</ol>
 		</>
 	);
---
+++ src/components/AddCategory.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+export const AddCategory = ({setCategories}) => {
+
+  const [inputValue, setInputValue] = useState('Naruto');
+
+  const onInputChange = (e) => {
+    setInputValue(e.target.value)
+    // console.log(e.target.value)
+  }
+
+  const onSubmit=( event ) => {
+    event.preventDefault();
+
+    //validación para que tenga mínimo 5 caracteres
+    if( inputValue.trim().length <= 5) return;
+
+    setCategories(categories => [...categories, inputValue])
+  }
+  
+  return (
+    <form onSubmit={event =>onSubmit(event) }>
+      <input 
+        type="text"
+        placeholder="Buscar Gif"
+        value={ inputValue }
+        onChange={ e => onInputChange(e) }
+      />
+      <button>Agregar categoria</button>
+    </form>
+  )
+}
