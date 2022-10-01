c4314a9cd4a8bdd4b3ad58d15c08c32ef72b174c
Abimael Lovera <abimaellovera@gmail.com>
2022-10-01 10:14:36 -0300

    Criando listas

--- src/GifApp.jsx
+++ src/GifApp.jsx
@@ -1,7 +1,21 @@
+import { useState } from 'react';
+
 export const GifApp = () => {
+	const [categories, setCategories] = useState(['One Punch', 'Dragon Ball']);
+	console.log(categories);
 	return (
 		<>
 			<h1>GifApp</h1>
+			<ol>
+				{
+					categories.map((category) => {
+						return <li key={category}>{category}</li>;
+					})
+				}
+				<li>1</li>
+				<li>2</li>
+				<li>3</li>
+			</ol>
 		</>
 	);
 };
