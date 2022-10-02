c4dff62ec85f9e78bef588845b48216e9a7fb846
Abimael Lovera <abimaellovera@gmail.com>
2022-10-01 21:23:23 -0300

    utilizando useEffect

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,14 +1,18 @@
+import { useEffect } from "react";
 import { getGifs } from "../helpers/getGifs";
 
 export const GifGrid = ({ category }) => {
 
-  getGifs(category);
+  useEffect(() => {
+    getGifs(category);
+  }, [])
+  
 
   const gifs = [1,2,3,4,5,]
   // aCcgjEvLm31bTMbLOVJQ5w1daf3u39m3
   return (
     <>
-      <h3>{ category }</h3>      
+      <h3>{ category }</h3>
     </>
   )
 }
